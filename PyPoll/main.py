a6cce599b6f8d52c8f16a8a2f895a4ba13083aac
Ashley Dixon <ashleydixon2012@gmail.com>
2020-10-01 00:00:42 -0600

    Code reads the header of the PyPoll file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,8 +5,13 @@ import csv
 #extracting the file path
 polls_csv = os.path.join("Resources", "election_data.csv")
 
-#read file and skip header
+#read file, print header, skip header
 with open ("Resources/election_data.csv", newline='') as file:
     csv_reader=csv.reader(file, delimiter=",")
-    header=next(csv_reader)
+    csv_header=next(csv_reader)
+    print(f"csvheader:{csv_header}")
+
+#created a list for candidates that had votes
+candidates_with_votes=[]
+
 
