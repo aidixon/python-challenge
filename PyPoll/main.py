02a2642a9318c4b3443e9caa64f92267c5ffc742
Ashley Dixon <ashleydixon2012@gmail.com>
2020-10-02 20:26:50 -0600

    Separated voters/candidates/county to break down the challenge

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,21 +13,28 @@ with open ("Resources/election_data.csv", newline='') as file:
 
     #vote count
     initial_votes = 0
-    adding_votes = votes+=1
+    
 
     #dictionary of candidates
     candidates_with_votes_dict= {}
 
-    #for Candidates in csv_reader:
-        
+    #voter analysis 
     for row in csv_reader:
-        Voter_ID = int(row[0])
+        Voter_ID = int(row[0]) #+ int(initial_votes)
+        Voter_ID = float(Voter_ID)
+        votes = Voter_ID #+= 1 #+ initial_votes
+        votes += 1
+        
+
+        #County
         County = str(row[1])
+        
+        #Candiate analysis
         Candidate = str(row[2])
-        #Voter_ID = float(Voter_ID)
+        
 
         #votes = Voter_ID + votes
-        print(f'TOTAL number of votes: {int(Voter_ID)}')
+        print(f'TOTAL number of votes: {int(initial_votes)}')
         
          
 
